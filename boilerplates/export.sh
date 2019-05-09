9fd3d5e75fa858169b4d9f1a28b2a778b37ee01d
Harish Subramanium <hsubra89@gmail.com>
2019-05-09 21:08:55 +1000

    [ENG-5756] Fix boilerplates for SDK
    - Boilerplates were not re-exported after its contents were changed previously
    - Update "export.sh" to ensure that builds are reproducible. ( Requires a dependency on GNU-Tar )

--- boilerplates/export.sh
+++ boilerplates/export.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Requires (GNU tar) 1.32 to support
+# reproducible tar builds
+
+set -eoux pipefail
+
 rm -rf ../src/assets/templates && mkdir -p ../src/assets/templates;
 
 for i in $(ls -d */ | grep -v dist | grep -v bundles); do
     echo ${i%%/};
-    tar --exclude node_modules --exclude .DS_Store -cv -C ./${i%%/} . | gzip -n > ../src/assets/templates/${i%%/}.tar.gz
+    gtar --mtime="1970-01-01" \
+         --sort=name \
+         --owner=0 --group=0 --numeric-owner \
+         --exclude node_modules --exclude .DS_Store \
+         --exclude dist --exclude __generated \
+         -cv -C ./${i%%/} . | gzip -n > ../src/assets/templates/${i%%/}.tar.gz
 done;
