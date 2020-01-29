46bb73ad06d6819208e87e2671fbd51c4272423a
Jess Ahrens <jahrens@skedulo.com>
2020-01-29 11:24:53 +1000

    Quotations and path fix

--- tools/upload-sdk-to-s3.sh
+++ tools/upload-sdk-to-s3.sh
@@ -11,15 +11,15 @@ SDK_DIST_PATH=$2
 AWS_BUCKET=$3
 
 for ext in exe AppImage; do
-  if [ -f $SDK_DIST_PATH/skedulo-sdk.$ext ] ; then
-    aws s3api put-object --bucket $AWS_BUCKET --key $SDK_FILENAME.$ext --body $SDK_DIST_PATH/skedulo-sdk.$ext --acl public-read
+  if [ -f "$SDK_DIST_PATH/skedulo-sdk.$ext" ] ; then
+    aws s3api put-object --bucket $AWS_BUCKET --key "$SDK_FILENAME.$ext" --body "$SDK_DIST_PATH/skedulo-sdk.$ext" --acl public-read
   fi
 done
 
 # Issues with macOS.15 and electron-builder require manual zipping
 # https://github.com/electron-userland/electron-builder/issues/4299
-if [ -d $SDK_DIST_PATH/mac/skedulo-sdk.app ] ; then
-  cd $SDK_DIST_PATH/mac
+if [ -d "$SDK_DIST_PATH/mac/skedulo-sdk.app" ] ; then
+  cd "$SDK_DIST_PATH/mac"
   zip -r skedulo-sdk-fix.zip skedulo-sdk.app
-  aws s3api put-object --bucket $AWS_BUCKET --key $SDK_FILENAME.zip --body $SDK_DIST_PATH/mac/skedulo-sdk-fix.zip --acl public-read
+  aws s3api put-object --bucket $AWS_BUCKET --key "$SDK_FILENAME.zip" --body skedulo-sdk-fix.zip --acl public-read
 fi
